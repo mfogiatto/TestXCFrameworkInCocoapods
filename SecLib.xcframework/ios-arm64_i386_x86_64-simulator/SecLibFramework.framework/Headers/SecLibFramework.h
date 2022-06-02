7c811615b7f62c05412aa263a0424702638fdb47
Matheus Fogiatto <matheus.fogiatto@vodafone.com>
2022-06-02 16:12:16 +0100

    Added SecLib and VPartnerLib frameworks

---
+++ SecLib.xcframework/ios-arm64_i386_x86_64-simulator/SecLibFramework.framework/Headers/SecLibFramework.h
@@ -0,0 +1,20 @@
+//
+//  SecLib.h
+//  SecLib
+//
+//  Created by Mani on 23/08/17.
+//  Copyright © 2017 Vodafone Group Services GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include "GZIP.h"
+#include "CommonCryptoSec.h"
+//! Project version number for SecLib.
+FOUNDATION_EXPORT double SecLibVersionNumber;
+
+//! Project version string for SecLib.
+FOUNDATION_EXPORT const unsigned char SecLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecLib/PublicHeader.h>
+
+
